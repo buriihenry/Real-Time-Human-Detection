af42745eeb7425127cc18ac846245c4e211a3048
buriihenry <buriihenry@gmail.com>
2023-01-01 17:51:09 +0300

    Lets go #2023 first project

--- app.py
+++ app.py
@@ -3,3 +3,14 @@ import imutils
 from flask import Flask, render_template
 
 # initialize the flask app
+app = Flask(__name__)
+
+# Intitilize the log desciptor and set SVM to pre-trained pedestrian detector
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+# Route the app to the home page
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html")
